ce5e9102d73a02664820460c226c16668d93463e
cameron <cameron.grams.2@gmail.com>
2018-08-21 22:15:08 -0400

    refine the look with materialize

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Landing from './components/Landing/Landing'; 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>GMAP Forum</h1> 
+          < Landing /> 
       </div>
     );
   }
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    return(
+        <div >
+            <h1>GMAP Landing page</h1>
+            <a class="btn-floating btn-large waves-effect waves-light orange"><i class="material-icons">add</i></a>
+        </div>
+    )
+}
+
+export default Landing; 
