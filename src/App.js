54bbde812dc09c8019b3b21a98456568b10356f6
cameron <cameron.grams.2@gmail.com>
2018-08-22 22:33:14 -0400

    work on the render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import {  Switch, Route } from 'react-router-dom'; 
+import { BrowserRouter as Router } from 'react-router-dom'; 
 import Landing from './components/Landing/Landing'; 
+import Themes from './components/Themes/Themes'; 
+import SingleTheme from './components/SingleTheme/SingleTheme'; 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          < Landing /> 
+      <div>  
+      <Router >    
+        <Switch >
+
+          <Route exact path="/" Component={ Landing } />
+
+          <Route exact path="/themes" Component={ Themes } />
+
+          <Route exact path="/single-theme" Component={ SingleTheme } />
+
+        </Switch>
+      </Router>
       </div>
     );
   }
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import './Landing.css';
+
 
 const Landing = () => {
+
     return(
-        <div >
+        <div class="landing-div" >
             <h1>GMAP Landing page</h1>
             <a class="btn-floating btn-large waves-effect waves-light orange"><i class="material-icons">add</i></a>
         </div>
